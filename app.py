d6fbcbac3583674e7d37f15e36336b728950c9c4
J2b2590 <31115299+J2b2590@users.noreply.github.com>
2019-12-09 13:41:47 -0600

    added book_search class and made output user friendly

--- app.py
+++ app.py
@@ -1,31 +1,4 @@
-import requests
-# from book_search import book_search
-
-
-# request module
-
-user_search = input("What kind of book are you looking for: ")
-# user search is letting the user query the api for their desired book
-response = requests.get("https://www.googleapis.com/books/v1/volumes?q=" + user_search)
-# response variable is making the api call to the book api
-response = response.json()
-# the json method is converting text into a data object to I can read it
-
-
-for item in response.get('items', [])[:5]:
-    if 'title' not in item['volumeInfo'] or 'authors' not in item['volumeInfo'] or 'publisher' not in item[
-        'volumeInfo']:
-
-        continue
-    # iterating over items in our api call and splicing to show only 5 data points
-    # and only showing item.volumeInfo.authors,item.volumeInfo.title, and item.volumeInfo.publisher
-
-    book_choice = "TITLE: %s,   "   "AUTHOR: %s,   "  "PUBLISHER: %s " % (
-        item['volumeInfo']['title'], item['volumeInfo']['authors'], item['volumeInfo']['publisher'])
-
-    print(book_choice)
-
-    # creating book_choice var to store data from the loop and printing desired information
+from book_search import book_search
 
 reading_list = []
 # created a variable with an empty array to hold the list of user_select books and adding
@@ -37,3 +10,4 @@ while True:
     reading_list.append(user_select)
     print("This is your reading list, ", reading_list)
 # showing the reading_list from the user_select books
+
--- book_search.py
+++ book_search.py
@@ -1,3 +1,25 @@
-class book_search:
+import requests
 
+
+# request module
+
+class book_search:
+    user_search = input("What kind of book are you looking for: ")
+    # user search is letting the user query the api for their desired book
+    response = requests.get("https://www.googleapis.com/books/v1/volumes?q=" + user_search)
+    # response variable is making the api call to the book api
+    response = response.json()
+    # the json method is converting text into a data object to I can read it
+
+    for item in response.get('items', [])[:5]:
+        if 'title' not in item['volumeInfo'] or 'authors' not in item['volumeInfo'] or 'publisher' not in item[
+            'volumeInfo']:
+            continue
+        # iterating over items in our api call and splicing to show only 5 data points
+        # and only showing item.volumeInfo.authors,item.volumeInfo.title, and item.volumeInfo.publisher
+
+        book_choice = "\n" "TITLE: %s,   " "\n"   "AUTHOR: %s,   " "\n"   "PUBLISHER: %s " % (
+            item['volumeInfo']['title'], item['volumeInfo']['authors'], item['volumeInfo']['publisher'])
+
+        print(book_choice)
 
