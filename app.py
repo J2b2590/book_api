abe898308c51109ea55aec548d6908e9cb531882
J2b2590 <31115299+J2b2590@users.noreply.github.com>
2019-12-10 17:49:46 -0600

    saved user_selected books to list and user can query for more books to add

--- app.py
+++ app.py
@@ -1,15 +1,33 @@
 from book_search import book_search
 
-book_search()
+
+book = book_search()
+
+# for book in book_choice:
+#     print(book)
 reading_list = []
 # created a variable with an empty array to hold the list of user_select books and adding
 while True:
-    user_select = input("Which book would you like? ")
+    book_choices = book.response_list
+    user_select = input("Which book would you like to add to your reading list? ")
+    user_select = book.validation(user_select, True)
+    for book_choice in book_choices:
+        if book_choice["title"] == user_select:
+            reading_list.append(book_choice)
     # creating an input for the user to choose what books to add to their reading_list
-    if not user_select:
-        break
-    reading_list.append(user_select)
-    print("This is your reading list, ", reading_list)
+
+    print("\n" "This is your reading list: ")
+    
+    for user_select in reading_list:
+        title = user_select['title']
+        author = user_select['author']
+        publisher = user_select['publisher']
+        saved_reading_list = "\n" "TITLE: %s,   " "\n"   "AUTHOR: %s,   " "\n"   "PUBLISHER: %s " "\n" % (
+            title, author, publisher)
+
+        print( saved_reading_list)
+
+    book.query_api()
 
 # showing the reading_list from the user_select books
 
--- book_search.py
+++ book_search.py
@@ -3,28 +3,42 @@ import requests
 
 class book_search:
 
-    @staticmethod
-    def validation(user_search):
-        if not user_search:
-            return False
-        return True
-    #     additional validation goes here
 
-    def __init__(self):
+    def validation(self,user_search,from_list = False):
+        # additional validation goes here
+        search_text = "You have entered an invalid book name, please try again "
+        while not user_search:
+            user_search = input(search_text)
+        if from_list:
+            while not any(book["title"] == user_search for book in self.response_list):
+                user_search = input(search_text)
+        return user_search
+
+    def query_api(self):
         self.user_search = input("What kind of book are you looking for: ")
-        while not self.validation(self.user_search):
-            self.user_search = input("You have entered an invalid book name, please try again ")
+        self.user_search = self.validation(self.user_search)
         self.response = requests.get('https://www.googleapis.com/books/v1/volumes?q=' + self.user_search)
         self.response = self.response.json()
+        self.response_list = []
         for item in self.response.get('items', [])[:5]:
             if 'title' not in item['volumeInfo'] or 'authors' not in item['volumeInfo'] or 'publisher' not in item[
                 'volumeInfo']:
                 continue
             # iterating over items in our api call and splicing to show only 5 data points
             # and only showing item.volumeInfo.authors,item.volumeInfo.title, and item.volumeInfo.publisher
+            title = item['volumeInfo']['title']
+            author = item['volumeInfo']['authors']
+            publisher = item['volumeInfo']['publisher']
 
             book_choice = "\n" "TITLE: %s,   " "\n"   "AUTHOR: %s,   " "\n"   "PUBLISHER: %s " "\n" % (
-                item['volumeInfo']['title'], item['volumeInfo']['authors'], item['volumeInfo']['publisher'])
+                title, author, publisher)
+
+            book = {"title": title, "author": author, "publisher": publisher}
+            self.response_list.append(book)
 
             print(book_choice)
+
+    def __init__(self):
+        self.query_api()
+
 
