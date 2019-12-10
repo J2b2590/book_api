d7377c895537fe3b499a4f19eac49d607f67022a
J2b2590 <31115299+J2b2590@users.noreply.github.com>
2019-12-10 15:23:47 -0600

    validation user input

--- book_search.py
+++ book_search.py
@@ -3,8 +3,17 @@ import requests
 
 class book_search:
 
+    @staticmethod
+    def validation(user_search):
+        if not user_search:
+            return False
+        return True
+    #     additional validation goes here
+
     def __init__(self):
         self.user_search = input("What kind of book are you looking for: ")
+        while not self.validation(self.user_search):
+            self.user_search = input("You have entered an invalid book name, please try again ")
         self.response = requests.get('https://www.googleapis.com/books/v1/volumes?q=' + self.user_search)
         self.response = self.response.json()
         for item in self.response.get('items', [])[:5]:
